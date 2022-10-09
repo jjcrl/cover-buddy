1b4a12a911b142ac1f7d8bb8ae1f867dd48e303a
jjcrl <j412cff@icloud.com>
2022-10-09 21:52:32 +0100

    basic set up of form , store, and submit handle

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,57 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { input } from "./lib/store";
+
+  const onSubmit = (e) => {
+    let prompt = JSON.stringify($input.prompt);
+    if (prompt.length > 10) {
+      console.log("ready to send off to api");
+    } else {
+      console.log("the prompt is not long enough");
+    }
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <section>
+    <form on:submit|preventDefault={onSubmit}>
+      <label for="prompt">Prompt:</label>
+      <textarea type="text" id="prompt" bind:value={$input.prompt} />
+      <button type="submit">Generate Cover letter</button>
+    </form>
+  </section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  form {
+    width: 70vw;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    background-color: whitesmoke;
+    border-radius: 15px;
+    align-items: center;
+    justify-content: space-between;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  label {
+    color: grey;
+    font-size: 3rem;
+    margin: auto auto 0 1.5rem;
+    font-weight: 600;
+    text-decoration: underline 5px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  textarea {
+    width: 90%;
+    height: 60%;
+    background-color: grey;
+    border: rgba(70, 70, 70, 0.861);
+    opacity: 50%;
+    border-radius: 10px;
+    margin: auto;
+    margin-bottom: 1.5rem;
+    font-size: 1.5rem;
+    resize: none;
+    padding: 1rem;
   }
 </style>
